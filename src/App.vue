9adfabeb9dde4c11f76dae51b2bc1c1fd48aab2b
Юрий Лисов <yulisov@fil-it.ru>
2021-12-20 19:04:55 +0300

    add blog title, content and checkboxes with preview

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <AddBlog />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AddBlog from './components/AddBlog.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    AddBlog
+  },
+  data() {
+    return {
+
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  font-family: 'Nunito SemiBold';
 }
 </style>
---
+++ src/components/AddBlog.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="add-blog">
+    <h2>Add a New Blog Post</h2>
+    <form>
+      <label for="title">Blog Title:</label>
+      <input type="text" id="title" v-model.lazy="blog.title" required>
+      <label for="blog-content">Blog Content:</label>
+      <textarea type="text" id="blog-content" v-model.lazy="blog.content"></textarea>
+      <div id="checkboxes">
+        <label for="ninjas">Ninjas</label>
+        <input type="checkbox" id="ninjas" value="ninjas" v-model="blog.categories">
+        <label for="wizards">Wizards</label>
+        <input type="checkbox" id="wizards" value="wizards" v-model="blog.categories">
+        <label for="mario">Mario</label>
+        <input type="checkbox" id="mario" value="mario" v-model="blog.categories">
+        <label for="cheese">Cheese</label>
+        <input type="checkbox" id="cheese" value="cheese" v-model="blog.categories">
+      </div>
+    </form>
+    <div id="preview">
+      <h3>Preview Blog</h3>
+      <p>Blog title: {{blog.title}}</p>
+      <p>Blog content:</p>
+      <p>{{blog.content}}</p>
+      <p>Blog Categories:</p>
+      <ul>
+        <li v-for="category in blog.categories" :key="category">{{category}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddBlog',
+  data() {
+    return {
+      blog: {
+        title: '',
+        content: '',
+        categories: []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+#add-blog *{
+    box-sizing: border-box;
+}
+#add-blog{
+    margin: 20px auto;
+    max-width: 500px;
+}
+label{
+    display: block;
+    margin: 20px 0 10px;
+}
+input[type="text"], textarea{
+    display: block;
+    width: 100%;
+    padding: 8px;
+}
+#preview{
+    padding: 10px 20px;
+    border: 1px dotted #ccc;
+    margin: 30px 0;
+}
+h3{
+    margin-top: 10px;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  margin-right: 20px;
+  cursor: pointer;
+}
+#checkboxes label {
+  display: inline-block;
+  cursor: pointer;
+}
+</style>
