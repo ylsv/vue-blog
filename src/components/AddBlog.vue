ed79cfe655e8c80c21e7b655370996875c2d007d
Юрий Лисов <yulisov@fil-it.ru>
2021-12-21 14:40:19 +0300

    add vue-resourse, addBlog post mock request

--- src/components/AddBlog.vue
+++ src/components/AddBlog.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="add-blog">
     <h2>Add a New Blog Post</h2>
-    <form>
+    <form v-if="!submitted">
       <label for="title">Blog Title:</label>
       <input type="text" id="title" v-model.lazy="blog.title" required>
       <label for="blog-content">Blog Content:</label>
@@ -20,7 +20,11 @@
       <select id="author" v-model="blog.author">
         <option v-for="author in authors" :key="author">{{author}}</option>
       </select>
+      <button @click.prevent="addBlog">Add Blog</button>
     </form>
+    <div v-if="submitted">
+      <h3>Thanks for adding your post</h3>
+    </div>
     <div id="preview">
       <h3>Preview Blog</h3>
       <p>Blog title: {{blog.title}}</p>
@@ -47,7 +51,17 @@ export default {
         author: '',
       },
       authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator'],
+      submitted: false,
     }
+  },
+  methods: {
+    addBlog(){
+      this.$http.post('http://jsonplaceholder.typicode.com/posts', {
+        title: this.blog.title,
+        body: this.blog.content,
+        userId: 1,
+      }).then(() => this.submitted = true)
+    },
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResoure from 'vue-resource'
 
 Vue.config.productionTip = false
+Vue.use(VueResoure)
 
 new Vue({
   render: h => h(App),
